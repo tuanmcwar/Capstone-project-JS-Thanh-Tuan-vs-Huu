33130d2b42162a37850be4e20c69a6e65ef1b6c5
tuanmcwar <tuanntch1@gmail.com>
2022-07-31 02:43:18 +0700

    edit detail js

--- controllers/detail.js
+++ controllers/detail.js
@@ -13,11 +13,72 @@ function showDetail(id) {
   promise.then(function (result) {
     console.log("hihi ", result.data.content);
     console.log("id", id);
-
+    getProductById(result.data.content);
+    relatedProducts(result.data.content.relatedProducts);
+    console.log(
+      "result.data.content.relatedProducts",
+      result.data.content.relatedProducts
+    );
     //Sau khi lấy dữ liệu từ backend về dùng dữ liệu đó tạo ra tr trên table
-    // renderProduct(result.data.content);
   });
   //Xử lý thất bại
   promise.catch(function (err) {});
 }
-function getProductById(id) {}
+function getProductById(product) {
+  let imge = "";
+  imge = product.image;
+  let html = `
+  <div class="image">
+            <img src="${product.image}" alt=""/>
+          </div>
+          <div class="product-info">
+            <h3 class="product-title">${product.name}</h3>
+            <p class="product-description">${product.description}
+            </p>
+            <h4>Available size</h4>
+            <div class="size">
+              <button class="btn-size">${product.size[0]}</button>
+              <button class="btn-size">${product.size[1]}</button>
+              <button class="btn-size">${product.size[2]}</button>
+              <button class="btn-size">${product.size[3]}</button>
+              <button class="btn-size">${product.size[4]}</button>
+              <button class="btn-size">${product.size[5]}</button>
+              <button class="btn-size">${product.size[6]}</button>
+              
+
+            </div>
+            <div class="price">${product.price}$</div>
+            <div class="volume">
+              <button class="plus">+</button>
+              <p class="number">1</p>
+              <button class="noplus">-</button>
+            </div>
+            <button class="add-card">Add to card</button>
+          </div>"`;
+
+  document.querySelector(".productDeteal").innerHTML = html;
+}
+function relatedProducts(arr) {
+  let html = "";
+
+  for (var i = 0; i < arr.length; i++) {
+    var pd = arr[i];
+    imge = pd.image;
+
+    html += `
+                <div id="item-Features" class="item">
+                    <img class="product-img" src="${imge}" alt="" />
+                    <h3 class="product-name">${pd.name}</h3>
+                    <p class="product-description">${pd.shortDescription}</p>
+                    <div class="card-bottom">
+                    <a href="../view/detail.html?productid=${pd.id}" class="btn-buy" onclick="showDetail('${pd.id}')">  Buy now</a>
+                       
+                        <p class="product-price">${pd.price}$</p>
+                    </div> 
+                </div> 
+          
+    
+        `;
+    document.querySelector(".product-item").innerHTML = html;
+  }
+}
