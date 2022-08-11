d006eb332337701958008edb67accf0dec77b328
usylyl0202 <nguyenhuu4497@gmail.com>
2022-08-12 00:21:08 +0700

    huu

--- model/Product.js
+++ model/Product.js
@@ -23,3 +23,18 @@ class Product {
     this.image = image;
   }
 }
+function Product () {
+  this.alias = '';
+  this.categories = '';
+  this.deleted = '';
+  this.description = '';
+  this.feature = '';
+  this.id = '';
+  this.image = '';
+  this.name = '';
+  this.price = '';
+  this.quantity = '';
+  this.relatedProducts = '';
+  this.shortDescription = '';
+  this.size = '';
+}
